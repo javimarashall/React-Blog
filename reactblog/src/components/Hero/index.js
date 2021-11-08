a4a89955df7ad272e489d9401f1fab5c94197fa7
javimarashall <jamondragon@ucdavis.edu>
2021-11-07 23:09:25 -0800

    navbar component done

--- reactblog/src/components/Hero/index.js
+++ reactblog/src/components/Hero/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import Card from '../UI/Card';
 import './style.css';
 import Logo from '../Logo/index'
+import Navbar from '../Navbar';
 
 const Hero = (props) => {
     return (
         <div>
             <Card>
+                <div style={{padding:'50px 0'}}>
                 <Logo />
+                </div>
+                <Navbar />
             </Card>
         </div>
     )
---
+++ reactblog/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = (props) => {
+    return (
+        <div className='navbar'>
+            Nav
+        </div>
+    )
+}
+export default Navbar;
