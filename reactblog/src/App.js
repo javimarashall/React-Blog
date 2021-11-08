c637c22486626e7d0b12d1ba2bfad181b685773e
javimarashall <jamondragon@ucdavis.edu>
2021-11-07 21:32:30 -0800

    finished header component

--- reactblog/src/App.js
+++ reactblog/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Home from './containers/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Hero />
+      <Home />
     </div>
   );
 }
---
+++ reactblog/src/components/Hero/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+const Hero = (props) => {
+    return (
+        <div>
+            Hero
+        </div>
+    )
+}
+export default Hero;
