4875cce6a7522d383c54d96424245e8ddd435c99
javimarashall <jamondragon@ucdavis.edu>
2021-11-08 22:39:38 -0800

    navbar done

--- reactblog/src/components/Navbar/index.js
+++ reactblog/src/components/Navbar/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './style.css';
+import search from '../../assets/search.png';
 
 const Navbar = (props) => {
     return (
         <div className='navbar'>
-            Nav
+            <ul className='navbarMenu'>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>About Us</a></li>
+                <li><a href='#'>Post</a></li>
+                <li><a href='#'>Contact Us</a></li>
+            </ul>
+            <div className='search'>
+                <input/>
+                <img src={search} alt='search icon'/>
+            </div>
         </div>
     )
 }
