20fa875500626396298785434b8c46b039fc2b6a
javimarashall <jamondragon@ucdavis.edu>
2021-11-10 23:32:18 -0800

    Home and contact-us components work with Router

--- reactblog/src/App.js
+++ reactblog/src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Home from './containers/Home';
+import Home from './containers/Home/index';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ContactUs from './containers/ContactUs/index';
 
 function App() {
   return (
-    <div className="App">
+  <Router>
+      <div className="App">
       <Header />
       <Hero />
-      <Home />
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/contact-us" element={<ContactUs/>} />
+      </Routes>
+      </div>
+  </Router>
+    
   );
 }
 
