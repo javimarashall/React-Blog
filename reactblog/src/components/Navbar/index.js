f2c964d373d5f5c028f5416db7cd3fed76a37342
javimarashall <jamondragon@ucdavis.edu>
2021-11-10 20:58:34 -0800

    search on navbar done

--- reactblog/src/components/Navbar/index.js
+++ reactblog/src/components/Navbar/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.css';
-import search from '../../assets/search.png';
+import searchPhoto from '../../assets/search.png';
 
 const Navbar = (props) => {
+    //sets the state for the search icon
+    const [search, setSearch] = useState(false);
+
+    //Sets the state of search from false to true
+    const openSearch = () => {
+        setSearch(true);
+    }
+
+    //dynamically create a class and use it as a variable in the input
+    const searchClass= search ? 'searchinput active' : 'searchinput';
+
+
+//function to handle the search form    
+    const submitSearch = (e) => {
+        e.preventDefault();
+        alert('searched');
+    } 
+
     return (
         <div className='navbar'>
             <ul className='navbarMenu'>
@@ -12,8 +30,10 @@ const Navbar = (props) => {
                 <li><a href='#'>Contact Us</a></li>
             </ul>
             <div className='search'>
-                <input/>
-                <img src={search} alt='search icon'/>
+                <form onSubmit={submitSearch}>
+                <input type='text' placeholder='Search' className={searchClass} />
+                <img src={searchPhoto} alt='Search Icon'className='searchIcon' onClick={openSearch} />
+                </form>
             </div>
         </div>
     )
