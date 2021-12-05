0ed06fa87765d7c59fa273d68f141e960896ce89
javimarashall <jamondragon@ucdavis.edu>
2021-12-04 22:54:08 -0800

    worked on the sidebar

--- reactblog/src/components/BlogPost/index.js
+++ reactblog/src/components/BlogPost/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './style.css';
 import Card from '../../components/UI/Card/index';
+import juniper from "../../blog-photos/IMG_3386.jpg"
 
 const BlogPost = (props) => {
     return (
         //style={{width: 70%}} being passed as a prop for style to the Card component
-       <div className='blogPostContainer'>
-           <Card>
-               <div className='blogHeader'>
+        <div className='blogPostContainer'>
+            <Card>
+                <div className='blogHeader'>
                     <span classname='blogCategory'>Featured</span>
                     <h1 className='postTitle'>Beautiful is Always Beautiful</h1>
-                    <span className='postedBy'>Posted on July 22, 2016 by Sora</span>
-               </div>
-           </Card>
-       </div>
+                    <span className='postedBy'>Posted on December 04, 2021 by Juniper</span>
+                </div>
+                <div className="postImageContainer">
+                    <img src={juniper} alt="Post Image" />
+                </div>
+            </Card>
+        </div>
 
     )
 }
--- reactblog/src/components/Sidebar/index.js
+++ reactblog/src/components/Sidebar/index.js
@@ -4,10 +4,12 @@ import Card from '../../components/UI/Card/index';
 
 const Sidebar = (props) => {
     return (
-        //style={{width: 27%}} being passed as a prop for style to the Card component
-        <Card style={{width: '27%'}}>
-            Sidebar
-        </Card>
+        <div className="sideBarContainer">
+            <Card>
+                Sidebar
+            </Card>
+        </div>
+
     )
 }
 
