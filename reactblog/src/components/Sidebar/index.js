f74c8e254d628e5de227016078c0d4ccd2592245
javimarashall <jamondragon@ucdavis.edu>
2021-12-19 22:16:31 -0800

    created recent post container

--- reactblog/src/components/Sidebar/index.js
+++ reactblog/src/components/Sidebar/index.js
@@ -5,8 +5,29 @@ import Card from '../../components/UI/Card/index';
 const Sidebar = (props) => {
     return (
         <div className="sideBarContainer">
-            <Card>
-                Sidebar
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                <div className='cardHeader'>
+                    <span>About Us</span>
+                </div>
+                <div className='profileImageContainer'>
+                    <img src="https://static.scientificamerican.com/sciam/cache/file/F27ED9D2-18FC-4836-BC4F2F5CE6630BB4_source.jpg?w=590&h=800&8D2E82D7-5354-4B3B-B12D591361A88D36" alt="jax" />
+                </div>
+                <div className='cardBody'>
+                    <p className='personalBio'>This is beer, this is going to be a blog about beer brewing</p>
+                </div>
+            </Card>
+
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                <div className='cardHeader'>
+                    <span>Social Network</span>
+                </div>
+            </Card>
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                <div className='cardHeader'>
+                    <span>Recent Posts</span>
+                </div>
+            
+
             </Card>
         </div>
 
--- reactblog/src/components/UI/Card/index.js
+++ reactblog/src/components/UI/Card/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const Card = (props) => {
     return (
         //...props being used to pass the style properties. style={props.style} also work
-        <div className='card' {...props}> 
+        <div className='card' style={{width: props.width ? props.width: '100%'}} {...props}> 
             {props.children}
         </div>
     )
